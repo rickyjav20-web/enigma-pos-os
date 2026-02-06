520b60da348c6adf6d99c60e24a10c9156aecfb8
Ricardo Mejia <rickyjav@Ricardos-MacBook-Air.local>
2026-02-06 10:41:32 -0400

    fix(staff): remove duplicate shebang from env.sh

--- apps/staff/env.sh
+++ apps/staff/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#!/bin/sh
+
 echo "Generating env-config.js..."
 echo "window._env_ = {" > /usr/share/nginx/html/env-config.js
 echo "  VITE_API_URL: \"$VITE_API_URL\"" >> /usr/share/nginx/html/env-config.js
