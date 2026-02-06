d0ee27bae9b70398d8c8c2d245700fdd1fc51aa6
Ricardo Mejia <rickyjav@Ricardos-MacBook-Air.local>
2026-02-06 10:57:35 -0400

    fix(staff): force manual execution of env script in CMD for reliability

--- apps/staff/env.sh
+++ apps/staff/env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "Generating env-config.js..."
 echo "window._env_ = {" > /usr/share/nginx/html/env-config.js
 echo "  VITE_API_URL: \"$VITE_API_URL\"" >> /usr/share/nginx/html/env-config.js
 echo "};" >> /usr/share/nginx/html/env-config.js
+echo "✅ env-config.js created:"
+cat /usr/share/nginx/html/env-config.js
 echo "Done."
