1355df61ef32f5b63795a811ec1fe47beed1469a
Ricardo Mejia <rickyjav@Ricardos-MacBook-Air.local>
2026-02-06 10:20:59 -0400

    fix(staff): standardize nginx entrypoint and debug env script

--- apps/staff/env.sh
+++ apps/staff/env.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#!/bin/sh
+echo "Generating env-config.js..."
 echo "window._env_ = {" > /usr/share/nginx/html/env-config.js
 echo "  VITE_API_URL: \"$VITE_API_URL\"" >> /usr/share/nginx/html/env-config.js
 echo "};" >> /usr/share/nginx/html/env-config.js
+echo "Done."
