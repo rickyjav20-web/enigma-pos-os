ce2e8fa8aac6251aed9097157fd38b27b810960b
Ricardo Mejia <rickyjav@MacBookAir.NUWE>
2026-02-06 22:34:46 -0400

    fix(staff): force rebuild and ensure clean nginx config (port 8080)

--- apps/staff/client/vite.config.js
+++ apps/staff/client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
+// Force Rebuild 2026-02-06
 export default defineConfig({
     plugins: [react()],
     resolve: {
